0616774c9d50a51436bcb8709118cc5e5ca5b03b
maral <artemgorodilov13@gmail.com>
2026-06-18 00:01:01 +1200

    Add files via upload

--- app.py
+++ app.py
@@ -1,83 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scardum.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(50), unique=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    badge = db.Column(db.String(50))
+    is_special = db.Column(db.Boolean, default=False)
+    short_text = db.Column(db.String(255))
+    
+    genre = db.Column(db.String(100))
+    release_date = db.Column(db.String(50))
+    publisher = db.Column(db.String(100))
+    metacritic = db.Column(db.String(20))
+    story = db.Column(db.Text)
+    requirements = db.Column(db.Text)
+    
+    fps_750ti = db.Column(db.String(100))
+    fps_1050ti = db.Column(db.String(100))
+    fps_1060 = db.Column(db.String(100))
+    fps_1660 = db.Column(db.String(100))
+    fps_rx570_580_4 = db.Column(db.String(100))
+    fps_rx580_8 = db.Column(db.String(100))
+
+    author_test = db.Column(db.Text)
+    author_settings = db.Column(db.String(50))
+    author_resolution = db.Column(db.String(50))
+    author_message = db.Column(db.Text)
+    
+    patch_instruction = db.Column(db.Text)
+    optimization = db.Column(db.Text)
+    worth_2026 = db.Column(db.Text)
+    summary = db.Column(db.Text)
 
 @app.route('/')
 def index():
-    return render_template('index.html', status="Разработка на localhost")
+    all_games = Game.query.all()
+    return render_template('index.html', games=all_games)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-# Добавили game/ перед каждым файлом:
-@app.route('/game/bfbc2')
-def bfbc2():
-    return render_template('game/bfbc2.html')
-
-@app.route('/game/crysis3')
-def crysis3():
-    return render_template('game/crysis3.html')
-
-@app.route('/game/witcher3')
-def witcher_page():
-    return render_template('game/witcher3.html')
-
-@app.route('/game/fallout4')
-def fallout4_page():
-    return render_template('game/fallout4.html')
-
-@app.route('/game/skyrim')
-def skyrim_page():
-    return render_template('game/skyrim.html')
-
-@app.route('/game/fallout3')
-def fallout3_page():
-    return render_template('game/fallout3.html')
-
-@app.route('/game/falloutnv')
-def falloutnv_page():
-    return render_template('game/falloutnv.html')
-
-@app.route('/game/fallout1997')
-def fallout1997_page():
-    return render_template('game/fallout1997.html')
-
-@app.route('/game/fallout2')
-def fallout2_page():
-    return render_template('game/fallout2.html')
-
-@app.route('/game/fallout76')
-def fallout76_page():
-    return render_template('game/fallout76.html')
-
-@app.route('/game/skyrimarena')
-def skyrimarena_page():
-    return render_template('game/skyrimarena.html')
-
-@app.route('/game/skyrim2')
-def skyrim2_page():
-    return render_template('game/skyrim2.html')
-
-@app.route('/game/skyrim3')
-def skyrim3_page():
-    return render_template('game/skyrim3.html')
-
-@app.route('/game/skyrim4')
-def skyrim4_page():
-    return render_template('game/skyrim4.html')
-
-@app.route('/game/skyrim4re')
-def skyrim4re_page():
-    return render_template('game/skyrim4re.html')
-
-@app.route('/game/skyrimse')
-def skyrimse_page():
-    return render_template('game/skyrimse.html')
-    
-@app.route('/game/skyrimae')
-def skyrimae_page():
-    return render_template('game/skyrimae.html')
+@app.route('/game/<game_slug>')
+def game_page(game_slug):
+    game = Game.query.filter_by(slug=game_slug).first()
+    if game:
+        return render_template('game_review.html', game=game)
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
